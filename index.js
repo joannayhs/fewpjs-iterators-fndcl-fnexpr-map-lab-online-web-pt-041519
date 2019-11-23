5afaec9f024f9438c267572d63b44ab06e6be836
joannayhs <joannayuhas@gmail.com>
2019-11-23 10:58:43 -0500

    Done.

--- index.js
+++ index.js
@@ -12,5 +12,10 @@ const tutorials = [
 ];
 
 const titleCased = () => {
-  return tutorials
+  return tutorials.map(line => {
+    let words = line.split(' ')
+    let capitals = words.map(word => word.charAt(0).toUpperCase() + word.slice(1))
+    let title = capitals.join(' ')
+    return title
+  })
 }
